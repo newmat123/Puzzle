5edf71cc4631056ea1a303b287c805f1323ee1e3
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-09-05 22:51:15 +0200

    idk

--- Puzzel_1/Assets/scripts/playerMovement.cs
+++ Puzzel_1/Assets/scripts/playerMovement.cs
@@ -7,21 +7,6 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
-
-
-        foreach (Touch touch in Input.touches)
-        {
-            if (touch.phase == TouchPhase.Began)
-            {
-                // Construct a ray from the current touch coordinates
-                Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                if (Physics.Raycast(ray))
-                {
-                    // Create a particle if hit
-                    
-                }
-            }
-        }
-
+        
     }
 }
