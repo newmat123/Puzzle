e0ece172842a29f9b3134eb88db5b1b3d8db3b64
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-09-06 11:49:15 +0200

    player movement
    en lille bug lol

--- Puzzel_1/Assets/scripts/playerMovement.cs
+++ Puzzel_1/Assets/scripts/playerMovement.cs
@@ -5,8 +5,48 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
 
+    
+
     void Update()
     {
-        
+        if (transform.position.x < 2.25 && transform.position.x > -2.3) {
+
+            if (Input.touchCount > 0)
+            {
+
+                Touch touch = Input.GetTouch(0);
+
+                Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+                touchPos.z = 0;
+                touchPos.y = -3;
+
+
+                transform.position = touchPos;
+                 
+            }
+
+
+        }else if (transform.position.x >= 2.25)
+        {
+
+            Vector3 back = transform.position;
+
+            back.x = 2.23f;
+
+            transform.position = back;
+
+        }
+        else if (transform.position.x <= -2.3)
+        {
+
+            Vector3 back = transform.position;
+
+            back.x = -2.28f;
+
+            transform.position = back;
+
+        }
+
     }
 }
