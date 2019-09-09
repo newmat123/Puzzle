e8265b58d63fc5c1c4734c54b122421392c24299
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-09-09 20:25:47 +0200

    playerMovement bug fix
    player movement done

--- Puzzel_1/Assets/scripts/playerMovement.cs
+++ Puzzel_1/Assets/scripts/playerMovement.cs
@@ -9,44 +9,31 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
-        if (transform.position.x < 2.25 && transform.position.x > -2.3) {
 
-            if (Input.touchCount > 0)
+
+        if (Input.touchCount > 0)
+        {
+
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(touchPos.x < 2.25 && touchPos.x > -2.3)
             {
-
-                Touch touch = Input.GetTouch(0);
-
-                Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
 
                 touchPos.z = 0;
                 touchPos.y = -3;
 
 
                 transform.position = touchPos;
-                 
+
             }
-
-
-        }else if (transform.position.x >= 2.25)
-        {
-
-            Vector3 back = transform.position;
-
-            back.x = 2.23f;
-
-            transform.position = back;
-
+                
+                 
         }
-        else if (transform.position.x <= -2.3)
-        {
 
-            Vector3 back = transform.position;
 
-            back.x = -2.28f;
-
-            transform.position = back;
-
-        }
+        
 
     }
 }
