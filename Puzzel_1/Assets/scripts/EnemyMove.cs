d9ac8a3f1ac1a175eabcb336a32cfedbfbfc9d8f
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-09-10 16:47:37 +0200

    health bar done

--- Puzzel_1/Assets/scripts/EnemyMove.cs
+++ Puzzel_1/Assets/scripts/EnemyMove.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    
 
     void Start()
     {
 
-        HealtBar hb = this.GetComponent<HealtBar>();
-
         Rigidbody2D RB = GetComponent<Rigidbody2D>();
 
         RB.velocity = new Vector2(0,-5);
-
-        //hb.SetSize(.4f);
 
     }
 
     void Update()
     {
         
-        
+        if(transform.position.y < -4)
+        {
+
+            FindObjectOfType<HealtBar>().SetSize(.2f);
+
+            Destroy(this);
+
+        }
 
     }
 }
