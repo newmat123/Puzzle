11f6624254df86fd6263bd2740a4fca30c66afb6
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-09-20 13:16:43 +0200

    charger

--- Puzzel_1/Assets/scripts/EnemySpawnScript.cs
+++ Puzzel_1/Assets/scripts/EnemySpawnScript.cs
@@ -9,12 +9,14 @@ public class EnemySpawnScript : MonoBehaviour
 
     public Vector3 spawnValues;
     public float spawnWait;
+    public float SpecialSpawnWait;
+
     public int startWait;
     public float A;
 
+    private float specialTimer;
     private float timer;
     private float timerB;
-    int randEnemy;
 
     void Start()
     {
@@ -25,6 +27,9 @@ public class EnemySpawnScript : MonoBehaviour
     public void startWaiter()
     {
 
+        SpecialSpawnWait = Random.Range(10, 15);
+        specialTimer = 0;
+
         StartCoroutine(WaitSpawner());
         timerB = 0;
         spawnWait = 2;
@@ -33,9 +38,29 @@ public class EnemySpawnScript : MonoBehaviour
 
     void Update()
     {
+        specialTimer += Time.deltaTime;
 
-      
-        
+        if (specialTimer > SpecialSpawnWait)
+        {
+
+            SpawnCharger();
+            
+            SpecialSpawnWait = Random.Range(5, 20);
+
+            specialTimer = 0;
+
+        }
+
+    }
+
+    public void SpawnCharger()
+    {
+
+        Vector3 spawnPoinrt = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+
+        Instantiate(Enemys[1], spawnPoinrt + transform.TransformPoint(0, 0, 0), transform.rotation);
+
+
     }
 
     IEnumerator WaitSpawner()
@@ -44,23 +69,21 @@ public class EnemySpawnScript : MonoBehaviour
 
         while (true)
         {
+
             timer += Time.deltaTime;
             timerB += Time.deltaTime;
 
-           
-
             if (timer >= spawnWait)
             {
-                randEnemy = Random.Range(0, Enemys.Length);
 
                 Vector3 spawnPoinrt = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 
-                Instantiate(Enemys[randEnemy], spawnPoinrt + transform.TransformPoint(0, 0, 0), transform.rotation);
+                Instantiate(Enemys[0], spawnPoinrt + transform.TransformPoint(0, 0, 0), transform.rotation);
 
                 yield return new WaitForSeconds(spawnWait);
 
                 timer = 0;
-                
+
                 if (spawnWait > 0.10f)
                 {
 
@@ -77,6 +100,7 @@ public class EnemySpawnScript : MonoBehaviour
                 {
                     spawnWait = 0.10f;
                 }
+
             }
 
         }
