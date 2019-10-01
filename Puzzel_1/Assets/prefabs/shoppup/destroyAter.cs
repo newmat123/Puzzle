e6cc7018c5d96774cbf131099d16a0556c0a3062
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-10-01 20:20:06 +0200

    working on pupup

---
+++ Puzzel_1/Assets/prefabs/shoppup/destroyAter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyAter : MonoBehaviour
+{
+
+    void Start()
+    {
+        Destroy(this.gameObject, 1);
+    }
+
+}
